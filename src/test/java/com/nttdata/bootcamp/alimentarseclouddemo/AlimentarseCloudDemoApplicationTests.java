0c10ee5c3c0da9a1df4835fc733ffe539ee7cc5a
Francisco Tomas Guzman Fernandez <francistomas96@gmail.com>
2021-10-22 10:20:04 +0200

    first commit

---
+++ src/test/java/com/nttdata/bootcamp/alimentarseclouddemo/AlimentarseCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.alimentarseclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimentarseCloudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
